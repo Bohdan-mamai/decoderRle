34f413839542f0b6cd4c759ad6466e583964b531
Bohdan_Mamai <b909mamay@gmail.com>
2020-04-17 09:39:12 +0300

    UnitTests

---
+++ src/main/java/main/java/DecoderRLEData.java
@@ -0,0 +1,51 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class DecoderRLEData {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        String decodedText = decoding(text);
+        System.out.println(decodedText);
+    }
+
+    public static String decoding(String inputData) {
+
+        String regex = "\\\\";
+
+        String regexNum = "[0-9]";
+
+        int count = 0;
+
+        StringBuilder result = new StringBuilder () ;
+
+        for (int i = 0; i < inputData.length(); i++) {
+
+            char firstChar = inputData.charAt(i);
+            char secondChar = inputData.charAt(++i);
+
+            String element = String.valueOf(firstChar);
+            String nextElement = String.valueOf(secondChar);
+
+            if (element.matches(regexNum)) {
+                count = count * 10 + firstChar - '0' ;
+            } else if(element.matches(regex) && nextElement.matches(regex)){
+                result.append(firstChar);
+            } else if(element.matches(regex) && nextElement.matches(regexNum)){
+                result.append(secondChar);
+            }
+                while (count > 0){
+                    result.append(secondChar);
+                    count--;
+                }
+        }
+        return result.toString();
+    }
+
+}
+
+
